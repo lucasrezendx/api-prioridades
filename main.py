975f94ac0abb5ea5aec8e39e73849e148de1ca16
Lucas Rezende <54600536+lucasrezendx@users.noreply.github.com>
2025-10-28 09:45:52 -0300

    Update main.py

--- main.py
+++ main.py
@@ -115,7 +115,9 @@ def listar_agencias():
 # ---------------------------------------
 # Iniciar servidor
 # ---------------------------------------
+# Garante que o banco existe mesmo no Render
+init_db()
+
 if __name__ == "__main__":
-    init_db()
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
